f1be03d7dedf7e9bcf6a275da805e5f8329ae587
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2020-08-29 00:47:52 +0300

    :sparkles: added post method to short url

--- server/index.js
+++ server/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const yup = require('yup');
+const { nanoid } = require('nanoid');
+const monk = require('monk');
+
+const schema = yup.object().shape({
+  slug: yup.string().trim().matches(/[\w\-]/i),
+  url: yup.string().trim().url().required(),
+});
 
 const app = express();
 
@@ -12,19 +20,46 @@ app.use(express.json());
 app.use(express.static('./public'));
 
 
-app.get('/url/:id', (req, res) => {
-  // TODO: get a short url by id
+// app.get('/url/:id', (req, res) => {
+//   // TODO: get a short url by id
+// });
+
+// app.get('/:id', (req, res) => {
+//   // TODO: redirect to url
+// });
+
+
+app.post('/url', async (req, res, next) => {
+  let { slug, url } = req.body;
+  try {
+    await schema.validate({
+      slug,
+      url,
+    })
+    if (!slug) {
+      slug = nanoid(5);
+    }
+    slug = slug.toLowerCase();
+    res.json({
+      slug, 
+      url
+    });
+  } catch (error) {
+    next(error); 
+  }
 });
 
-app.get('/:id', (req, res) => {
-  // TODO: redirect to url
+app.use((error, req, res, next) => {
+  if (error.status) {
+    res.status(error.status);
+  } else {
+    res.status(500);
+  }
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? '❌' : error.stack,
+  });
 });
-
-
-app.post('/url', (req, res) => {
-  // TODO: create a short url
-});
-
 
 PORT = process.env.PORT || 9999;
 
