0fdb1403ad67ee783c5153e87cf8f50c9ec48aa8
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2020-08-28 23:51:21 +0300

    :zap: updated gitignore

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Piserver - short URLs'
+  });
+});
+
+PORT = process.env.PORT || 9999;
+
+app.listen(PORT, () => {
+  console.log(`Listening at http://localhost:${PORT}`);
+});
