7be336eaa5c99a35c886fafb6efc4b96d6f073bc
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2020-08-29 00:00:07 +0300

    :zap: created get/post paths

--- server/index.js
+++ server/index.js
@@ -9,13 +9,22 @@ app.use(helmet());
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
+app.use(express.static('./public'));
 
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Piserver - short URLs'
-  });
+app.get('/url/:id', (req, res) => {
+  // TODO: get a short url by id
 });
+
+app.get('/:id', (req, res) => {
+  // TODO: redirect to url
+});
+
+
+app.post('/url', (req, res) => {
+  // TODO: create a short url
+});
+
 
 PORT = process.env.PORT || 9999;
 
