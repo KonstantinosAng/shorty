fc62bede1f05c16f302c89561cea48263a6b54b1
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2022-11-14 14:07:11 +0200

    feat(netlify): - deploy using netlify serverless functions

---
+++ server/netlify/functions/index.js
@@ -0,0 +1,92 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const yup = require("yup");
+const { nanoid } = require("nanoid");
+const monk = require("monk");
+
+const schema = yup.object().shape({
+  slug: yup
+    .string()
+    .trim()
+    .matches(/[\w\-]/i),
+  url: yup.string().trim().url().required(),
+});
+
+require("dotenv").config();
+const db = monk(process.env.MONGODB_URI);
+const urls = db.get("urls");
+urls.createIndex({ slug: 1 }, { unique: true });
+
+const basePath = "";
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("./public"));
+
+app.get(`${basePath}/:id`, async (req, res, next) => {
+  const { id: slug } = req.params;
+  try {
+    const url = await urls.findOne({ slug });
+    if (url) {
+      res.redirect(url.url);
+    } else {
+      res.redirect(`/?error=${slug} not found`);
+    }
+  } catch (error) {
+    res.redirect(`/?error=Link not found`);
+  }
+});
+
+app.post(`${basePath}/url`, async (req, res, next) => {
+  let { slug, url } = req.body;
+  try {
+    await schema.validate({
+      slug,
+      url,
+    });
+    if (!slug) {
+      slug = nanoid(5);
+    } else {
+      const existing = await urls.findOne({ slug });
+      if (existing) {
+        throw new Error("Slug in use.");
+      }
+    }
+    slug = slug.toLowerCase();
+    const newUrl = {
+      url,
+      slug,
+    };
+    const created = await urls.insert(newUrl);
+    res.json(JSON.stringify(newUrl));
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((error, req, res, next) => {
+  if (error.status) {
+    res.status(error.status);
+  } else {
+    res.status(500);
+  }
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === "production" ? "❌" : error.stack,
+  });
+});
+
+const PORT = process.env.PORT || 9999;
+
+app.listen(PORT, () => {
+  console.log(`Listening at http://localhost:${PORT}${basePath}`);
+});
+
+module.exports.handler = serverless(app);
